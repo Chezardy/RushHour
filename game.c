cfabc1d346954ec4124674da240932c84b9ed2c2
Chezardy <arno33370@hotmail.fr>
2016-02-17 12:23:56 +0100

    ajout de game.c incomplet

---
+++ game.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "game.h"
+#include <assert.h>
+
+typedef SIZE_GAME 6
+
+/*typedef struct pos{
+	int x;
+	int y;
+}pos;*/
+
+struct game_s {
+	piece *tab;
+	int nbMouv;
+};
+
+game new_game_hr (int nb_pieces, piece *pieces){
+	game g = malloc(SIZE_GAME*sizeof(struct game_s);
+	g->tab = malloc(nb_pieces*sizeof(struct piece_s));
+	for(int i=0;i<nb_pieces;i++)
+		g->tab[i] = pieces[i];
+	g->nbMouv = 0;
+}
+
+void delete_game (game g){
+	free(g->tab);
+	free(g);
+}
+
+void copy_game (cgame src, game dst){
+	int nSrc = sizeof(src->tab)/sizeof(piece);
+	int nDst = sizeof(dst->tab)/sizeof(piece);
+	assert(nSrc != nDst);
+
+	for(int i=0;i<nDst)
+		dst->tab[i] = src->tab[i]; 
+	dst->nbMouv = src->nbMouv;
+}
+/*
+int game_nb_pieces(cgame g){
+	return sizeof(g->tab)/sizeof(piece);
+}
+*/
