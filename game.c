1dcee2b4dc02fb27623f66050a020d505b1e3912
Chezardy <arno33370@hotmail.fr>
2016-02-18 17:40:08 +0100

    ajout de fonctions dans game.c (play_move incomplet)

--- game.c
+++ game.c
@@ -5,14 +5,9 @@
 
 typedef SIZE_GAME 6
 
-/*typedef struct pos{
-	int x;
-	int y;
-}pos;*/
-
 struct game_s {
 	piece *tab;
-	int nbMouv;
+	int nb_mouv;
 };
 
 game new_game_hr (int nb_pieces, piece *pieces){
@@ -20,7 +15,8 @@ game new_game_hr (int nb_pieces, piece *pieces){
 	g->tab = malloc(nb_pieces*sizeof(struct piece_s));
 	for(int i=0;i<nb_pieces;i++)
 		g->tab[i] = pieces[i];
-	g->nbMouv = 0;
+	g->nb_mouv = 0;
+	return g;
 }
 
 void delete_game (game g){
@@ -35,7 +31,7 @@ void copy_game (cgame src, game dst){
 
 	for(int i=0;i<nDst)
 		dst->tab[i] = src->tab[i]; 
-	dst->nbMouv = src->nbMouv;
+	dst->nb_mouv = src->nb_mouv;
 }
 
 int game_nb_pieces(cgame g){
@@ -45,3 +41,60 @@ int game_nb_pieces(cgame g){
 cpiece game_piece(cgame g, int piece_num){
 	return g->tab[piece_num];
 }
+
+bool game_over_hr(cgame g){
+	return get_x(g->tab[0])==4 && get_y(g->tab[0]==3);
+}
+
+/**
+ * Fonction incomplète, il faut reproduire les actions du "case LEFT" pour les autres "case" (Seulement si c'est juste).
+**/
+bool play_move(game g, int piece_num, dir d, int distance){
+
+	piece tmp_piece = new_piece_rh(0,0,true,true); // Initialisation d'une pièce temporaire (mallocs)
+	copy_piece(g->tab[piece_num],tmp_piece); //copie de la pièce en paramètre dans tmp_piece
+
+	switch(d){
+		case LEFT:
+			if(get_x(tmp_piece)-distance < 0 || !is_horizontal(tmp_piece)) return false; // Si la pièce (+ la distance) déborde du jeu sur la gauche OU que la pièce est verticale, retourner false.
+
+			for(int i=0;i<game_nb_pieces(g);i++){
+				if(i==piece_num) continue; // ----/!\ Pour ne pas comparer la pièce à elle-même, mais je ne sais pas si on peut faire comme ceci. /!\----
+
+				for(int j=1;j<=distance;j++){
+					move_piece(tmp_piece,LEFT,1); //On déplace tmp_piece de 1 vers la gauche
+					if(intersect(tmp_piece,g->tab[i]) return false; // Si la pièce (+ la distance) chevauche une autre pièce, retourner false.
+				}
+			}
+			move_piece(g->tab[piece_num],LEFT,distance);
+			g->nb_mouv++;
+			break;
+
+		case RIGHT:
+			if(get_x(*p_piece)+distance+get_width(*p_piece) >= SIZE_GAME || !is_horizontal(*p_piece)) return false;
+			break;
+
+		case UP:
+			if(get_y(*p_piece)+distance+get_heigth(*p_piece) >= SIZE_GAME || is_horizontal(*p_piece)) return false;
+			break;
+
+		case DOWN:
+			if(get_y(*p_piece)-distance < 0 || is_horizontal(*p_piece)) return false;
+			break;
+	}
+	delete_piece(tmp_piece); // Supression de la pièce temporaire (free)
+	return true; //
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
