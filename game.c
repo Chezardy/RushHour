8d4ce0d1c811675c7b0169cf216317c44ea7fdb9
Chezardy <arno33370@hotmail.fr>
2016-02-19 19:19:29 +0100

    correction d'erreures de compilation dans game.c

--- game.c
+++ game.c
@@ -3,7 +3,7 @@
 #include "game.h"
 #include <assert.h>
 
-typedef SIZE_GAME 6
+#define SIZE_GAME 6
 
 struct game_s {
 	piece *tab;
@@ -11,8 +11,8 @@ struct game_s {
 };
 
 game new_game_hr (int nb_pieces, piece *pieces){
-	game g = malloc(SIZE_GAME*sizeof(struct game_s);
-	g->tab = malloc(nb_pieces*sizeof(struct piece_s));
+	game g = malloc(SIZE_GAME*sizeof(struct game_s));
+	g->tab = malloc(nb_pieces*sizeof(struct piece_s*));
 	for(int i=0;i<nb_pieces;i++)
 		g->tab[i] = pieces[i];
 	g->nb_mouv = 0;
@@ -29,7 +29,7 @@ void copy_game (cgame src, game dst){
 	int nDst = sizeof(dst->tab)/sizeof(piece);
 	assert(nSrc != nDst);
 
-	for(int i=0;i<nDst)
+	for(int i=0;i<nDst;i++)
 		dst->tab[i] = src->tab[i]; 
 	dst->nb_mouv = src->nb_mouv;
 }
@@ -43,7 +43,7 @@ cpiece game_piece(cgame g, int piece_num){
 }
 
 bool game_over_hr(cgame g){
-	return get_x(g->tab[0])==4 && get_y(g->tab[0]==3);
+	return get_x(g->tab[0])==4 && get_y(g->tab[0])==3;
 }
 
 /**
@@ -59,31 +59,35 @@ bool play_move(game g, int piece_num, dir d, int distance){
 			if(get_x(tmp_piece)-distance < 0 || !is_horizontal(tmp_piece)) return false; // Si la pièce (+ la distance) déborde du jeu sur la gauche OU que la pièce est verticale, retourner false.
 
 			for(int i=0;i<game_nb_pieces(g);i++){
-				if(i==piece_num) continue; // ----/!\ Pour ne pas comparer la pièce à elle-même, mais je ne sais pas si on peut faire comme ceci. /!\----
+				if(i==piece_num) continue; // Pour ne pas comparer la pièce à elle-même.
 
 				for(int j=1;j<=distance;j++){
 					move_piece(tmp_piece,LEFT,1); //On déplace tmp_piece de 1 vers la gauche
-					if(intersect(tmp_piece,g->tab[i]) return false; // Si la pièce (+ la distance) chevauche une autre pièce, retourner false.
+					if(intersect(tmp_piece,g->tab[i])) return false; // Si la pièce (+ la distance) chevauche une autre pièce, retourner false.
 				}
 			}
 			move_piece(g->tab[piece_num],LEFT,distance);
-			g->nb_mouv++;
+			g->nb_mouv+=distance;
 			break;
 
 		case RIGHT:
-			if(get_x(*p_piece)+distance+get_width(*p_piece) >= SIZE_GAME || !is_horizontal(*p_piece)) return false;
+			if(get_x(tmp_piece)+distance+get_width(tmp_piece) >= SIZE_GAME || !is_horizontal(tmp_piece)) return false;
 			break;
 
 		case UP:
-			if(get_y(*p_piece)+distance+get_heigth(*p_piece) >= SIZE_GAME || is_horizontal(*p_piece)) return false;
+			if(get_y(tmp_piece)+distance+get_height(tmp_piece) >= SIZE_GAME || is_horizontal(tmp_piece)) return false;
 			break;
 
 		case DOWN:
-			if(get_y(*p_piece)-distance < 0 || is_horizontal(*p_piece)) return false;
+			if(get_y(tmp_piece)-distance < 0 || is_horizontal(tmp_piece)) return false;
 			break;
 	}
 	delete_piece(tmp_piece); // Supression de la pièce temporaire (free)
 	return true; //
+}
+
+int game_nb_moves(cgame g){
+	return g->nb_mouv;
 }
 
 
