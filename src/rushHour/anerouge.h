6c653cf4dead05be416c6c3ad18f4f5a5db0bb20
ebarjou <e.barjousuire@gmail.com>
2016-03-11 09:45:27 +0100

    Update anerouge.h

--- src/rushHour/anerouge.h
+++ src/rushHour/anerouge.h
@@ -6,6 +6,7 @@ Creer un jeu de taille 5*5 et y place les pieces de l'ane rouge
 
 Il faudra aussi modifier les fonction d'affichage pour que les bords au et bas des tableaux fassent la bonne taille
 	et trouver une moyen de régler le problème de fin de partie entre les jeux.
+	(+ placer la sortie a la bonne place dans gridDisplay et simpleDisplay, et eventuellement changer les couleurs dans gridDisplay)
 */
 game AR_getGame();
 
