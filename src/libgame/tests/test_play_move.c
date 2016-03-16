d3f297e7c1d1005750feae9d1a71f105276777a7
Chezardy <arno33370@hotmail.fr>
2016-03-16 12:54:13 +0100

    adaptation test_play_move.c aux nouvelles fonctions

--- src/libgame/tests/test_play_move.c
+++ src/libgame/tests/test_play_move.c
@@ -21,12 +21,12 @@ int main(int argc, char *argv[]){
 
 	int nb = 4;
 	piece pieces[nb];
-	pieces[0] = new_piece_rh(1,3,true,true);
-	pieces[1] =	new_piece_rh(5,3,false,false);
-	pieces[2] =	new_piece_rh(0,0,true,true);
-	pieces[3] =	new_piece_rh(3,4,false,true);
+	pieces[0] = new_piece(1,3,2,1,true,false);
+	pieces[1] =	new_piece(5,3,1,3,false,true);
+	pieces[2] =	new_piece(0,0,2,1,true,false);
+	pieces[3] =	new_piece(3,4,2,1,true,false);
 
-	game g = new_game_hr (nb, pieces);
+	game g = new_game(6,6,nb, pieces);
 
  	if(play_move(g,1,1,1)) return EXIT_FAILURE;
  	if(play_move(g,3,3,1)) return EXIT_FAILURE;
